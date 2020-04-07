122df7b3ca49c75f709e733eab375a85d371fce0
Evgeny Karasev <gnomskg@gmail.com>
2020-04-07 15:53:26 +0300

    fix: fix  calculate crs in map component

--- src/components/Map/index.jsx
+++ src/components/Map/index.jsx
@@ -12,13 +12,13 @@ import MapLayers from '../MapLayers';
 const Map = () => {
   const { loading, error, data } = useQuery(GET_MAP_OPTIONS);
 
-  if (loading) return <p>Loading...</p>;
-  if (error) return <p>Error</p>;
+  if (loading || error) return null;
 
   const {
     position,
     substrate: { service: subService, crs: subCrs, options: subOptions }
   } = data;
+  const crs = subCrs === '3857' ? L.CRS.EPSG3857 : L.CRS.EPSG4326;
   return (
     <LeafletMap
       id="root-map"
@@ -27,14 +27,14 @@ const Map = () => {
       center={position.origin}
       zoom={0}
       zoomControl={false}
-      crs={L.CRS.EPSG3857}
+      crs={crs}
     >
       {subService === 'wms' ? (
         <WMSTileLayer
           url={subOptions.url}
           layers={subOptions.layers}
           tiled={subOptions.tiled}
-          crs={subCrs === '3857' ? L.CRS.EPSG3857 : L.CRS.EPSG4326}
+          crs={crs}
         />
       ) : null}
       <FeatureGroup>
